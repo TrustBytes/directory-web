11d135ea8560ae076cc643433cc6f91a8cb4309c
twopats <lpatipat@icloud.com>
2023-10-14 01:14:39 -0400

    fix: mock data fetching

--- apps/web/app/_utils/mock.ts
+++ apps/web/app/_utils/mock.ts
@@ -2,7 +2,7 @@ import { promises as fs } from "node:fs"
 
 async function getMockUsers(): Promise<User[]> {
   try {
-    const data: string = await fs.readFile(`${process.cwd()}  /app/_data/mock-users.json`, "utf-8")
+    const data: string = await fs.readFile(`${process.cwd()}/app/_data/mock-users.json`, "utf-8")
     const dataParsed: Data = JSON.parse(data) as Data
     return dataParsed.users
   } catch (error) {
