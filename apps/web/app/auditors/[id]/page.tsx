fc59e55c87f0cc7f80d48c64930e4cb9d4f8b0ec
twopats <lpatipat@icloud.com>
2023-10-13 20:12:14 -0400

    migrate existing work from @twopats gh

---
+++ apps/web/app/auditors/[id]/page.tsx
@@ -0,0 +1,45 @@
+
+import React from 'react'
+import getMockUsers from '../../utils/mock';
+
+type AuditorPageParams = {
+  id: string
+}
+
+
+type ClassName = React.HTMLAttributes<HTMLHeadingElement>['className']
+
+
+const CSS_SECTION_LABEL: ClassName = 'text-2xl font-bold border-b py-2'
+const CSS_SECTION: ClassName = 'my-4 h-96 overflow-x-auto'
+
+export async function generateStaticParams() {
+  const mockUsers = await getMockUsers()
+  console.log(mockUsers)
+  return mockUsers.map((user) => ({
+    id: String(user.id),
+  }))
+}
+
+
+const page = ({ params }: { params: AuditorPageParams }) => {
+  const { id } = params;
+  return (
+    <div className='m-4'>
+      <section className={CSS_SECTION}>
+        <h1 className={CSS_SECTION_LABEL}>User {id} Overview</h1>
+      </section>
+      <section className={CSS_SECTION}>
+        <h2 className={CSS_SECTION_LABEL}>Credentials</h2>
+      </section>
+      <section className={CSS_SECTION}>
+        <h2 className={CSS_SECTION_LABEL}>Similar Profiles</h2>
+      </section>
+    </div>
+  )
+}
+
+// css classes
+
+
+export default page
