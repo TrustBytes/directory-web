d89e7e6ca38a0c97071b48b0fb7704e4c567a250
twopats <lpatipat@icloud.com>
2023-10-21 15:02:44 -0400

    fix: tableland table reads cache revalidations

--- apps/web/app/_lib/tableland/read.ts
+++ apps/web/app/_lib/tableland/read.ts
@@ -7,15 +7,41 @@ const AUDITOR_TABLE_NAME = "trustbytes_auditors_list_80001_7927";
 
 //@TODO: double check for run time errors
 async function getAllRegisteredAuditors(): Promise<TablelandAuditor[]> {
-	const { results } = await db.prepare(`SELECT * FROM ${AUDITOR_TABLE_NAME};`).all();
-	return results as TablelandAuditor[];
+	const { body } = await fetch("https://testnets.tableland.network/api/v1/query?mode=objects&statement=select%20*%20from%20trustbytes_auditors_list_80001_7927", {
+		next: { revalidate: 60 }
+	})
+
+	const decoder = new TextDecoder('utf-8')
+	// const { results } = await db.prepare(`SELECT * FROM ${AUDITOR_TABLE_NAME};`).all();
+	// console.log("tableland auditors: ", results)
+	if (!body) {
+		throw new Error("Error getting all auditors");
+	}
+	const reader = body.getReader()
+
+
+	let result = ''
+
+	for (; ;) {
+		/* eslint-disable no-await-in-loop -- can use await in this use case of reading data streams */
+		const { done, value } = await reader.read()
+		if (done) {
+			break;
+		}
+		result += decoder.decode(value)
+	}
+
+	const data: TablelandAuditor[] = (JSON.parse(result))
+
+	return data
+	// return body as TablelandAuditor[];
 }
 
 //@TODO: double check for run time errors & fix sql
 async function getRegisteredAuditor(_address: string): Promise<TablelandAuditor> {
 	// try {
-		const { results } = await db.prepare(`SELECT * FROM ${AUDITOR_TABLE_NAME} WHERE address = X'${"07f093df2C3E4BA793dbb3877508B1B2b0BAE9F0"}';`).all();
-		return results as unknown as TablelandAuditor;
+	const { results } = await db.prepare(`SELECT * FROM ${AUDITOR_TABLE_NAME} WHERE address = X'${"07f093df2C3E4BA793dbb3877508B1B2b0BAE9F0"}';`).all();
+	return results as unknown as TablelandAuditor;
 	// } catch (e) {
 	// 	console.error(e)
 	// 	throw new Error(`Error getting auditor with address ${address}`);
